8b08474c1cd57cceb000cbd98d00b9ef2c212dfc
Jonathan Vickers <jrvickers86@gmail.com>
2015-07-27 13:42:39 +0100

    adding simple cache-bust example

---
+++ cache-bust/grunt/cacheBust.js
@@ -0,0 +1,17 @@
+﻿module.exports = {
+
+        options: {
+            encoding: 'utf8',
+            algorithm: 'md5',
+            length: 16,
+            baseDir: '',
+            rename: false,
+            ignorePatterns: ["nochange"]
+        },
+        assets: {
+            files: [{
+                src: ['*.html']
+            }]
+        }
+    
+};
---
+++ cache-bust/grunt/watch.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+	css: {
+	    files: "css/*.css",
+	    tasks: ["cacheBust"],
+	    options: {
+
+	    }
+
+	},
+	scripts: {
+	    files: "js/*.js",
+	    tasks: ["cacheBust"],
+	    options: {
+
+	    }
+
+	}
+};
