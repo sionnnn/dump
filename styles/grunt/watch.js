5da7298deea7f42f8f2008b3fb5427a3ba70fabc
Michael Ord <michael.ord@gmail.com>
2015-07-30 12:23:24 +0100

    Added base css modules

--- styles/grunt/watch.js
+++ styles/grunt/watch.js
@@ -1,6 +1,6 @@
 module.exports = {
 	styles:{
-		files: ['<%= paths.assets.styles.dir %>/**/*.scss', 'bower_components/**/*.scss'],
+		files: ['<%= paths.assets.styles.dir %>/**/*.scss', '**/modules/**/*.scss'],
 	    tasks: ['styles'],
 	    options: {
 	      spawn: false,
